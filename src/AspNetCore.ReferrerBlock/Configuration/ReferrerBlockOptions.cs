67bda0e7f3cacb451d09b61e7009fdf97027b15f
Christophe Peugnet <christophe.peugnet@outlook.fr>
2025-11-22 12:23:55 +0100

    v1.0

--- src/AspNetCore.ReferrerBlock/Configuration/ReferrerBlockOptions.cs
+++ src/AspNetCore.ReferrerBlock/Configuration/ReferrerBlockOptions.cs
@@ -22,6 +22,7 @@ public class ReferrerBlockOptions
         "jbuif",
         "karyapemuda",
         "kawruhbasa",
+        "kaysdogs",
         "missywilkinson",
         "parakerja",
         "pvdwatersports",
